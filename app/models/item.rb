8bafca5cebda15f1277e7800e0793155c36bfb0d
mionpuuu <mionikeya@gmail.com>
2025-10-13 15:33:59 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,28 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :schedule_delivery_id, presence: true
-  validates :price, presence: true
-  validates :user, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :schedule_delivery
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :schedule_delivery_id
+  end
 end
