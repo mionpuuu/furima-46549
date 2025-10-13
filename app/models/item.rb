2db3512261f64ee001a0dfb59f95df3ec734e3a7
mionpuuu <mionikeya@gmail.com>
2025-10-13 16:58:45 +0900

    金額のバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,4 +25,12 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :schedule_delivery_id
   end
+
+  validates :price,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999,
+              message: 'must be half-width numbers'
+            }
 end
