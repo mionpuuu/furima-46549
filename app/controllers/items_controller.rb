9366de57ecb0f16bd38086703b245cd75a4c24d4
mionpuuu <mionikeya@gmail.com>
2025-10-13 15:56:24 +0900

    コントローラーアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
+    @items = Items.all
   end
 
   def new
+    @item = Items.new
   end
 
   def create
+    @item = Item.new(item.params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :image, :category_id,
+      :status_id, :shipping_fee_id, :prefecture_id,
+      :schedule_delivery_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
