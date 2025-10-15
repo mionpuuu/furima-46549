79478f7e4d7177c542fe1e5d933cdbb6bedd3799
mionpuuu <mionikeya@gmail.com>
2025-10-15 17:00:36 +0900

    pry-railsの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,14 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
   end
+
+  def create
+    binding.pry
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 end
