11bde2d20b2d744b71af3bf717e22f5a71df884f
mionpuuu <mionikeya@gmail.com>
2025-10-15 17:24:05 +0900

    ordersコントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
     @order_address = OrderAddress.new
   end
 
   def create
-    binding.pry
-    @order_address = OrderAddress.new(order_address_params)
+    @order_address = OrderAddress.new(order_params)
     if @order_address.save
       redirect_to root_path
     else
       render :index, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(
+      :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 end
